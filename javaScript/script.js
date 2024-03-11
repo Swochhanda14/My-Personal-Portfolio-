3272e0709759c622b7cf45f1165ba57ba12e1e22
Swochhanda14 <swochhanda14@gmail.com>
2024-03-11 21:14:46 +0545

    fift edit

---
+++ javaScript/script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".typing", {
+    strings: ["SwocXanda Karanjeet"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Graphic Designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
